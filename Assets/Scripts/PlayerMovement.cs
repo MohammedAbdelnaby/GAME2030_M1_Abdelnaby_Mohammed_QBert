d0ef75436a4482438004ee4150379335ad9bec81
Mohammed <mohammed.abdelnaby@georgebrown.ca>
2022-03-01 03:43:39 -0500

    added smooth jumping to q*bert and offsetting too

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0.0f, 2.0f)]
+    public float X = 0;
+
+    [Range(0.0f, 5.0f)]
+    public float Y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown("right"))
+        //{
+        //    GetComponent<Rigidbody2D>().velocity = new Vector2(0.8f, 2);
+        //}
+        //if (Input.GetKeyDown("left"))
+        //{
+        //    GetComponent<Rigidbody2D>().velocity = new Vector2(-0.65f, 4.8f);
+        //}
+        //if (Input.GetKeyDown("down"))
+        //{
+        //    GetComponent<Rigidbody2D>().velocity = new Vector2(-0.9f, 2);
+        //}
+
+
+        MoveUpRight();
+        MoveUpLeft();
+        MoveDownRight();
+        MoveDownLeft();
+        Reset();
+    }
+
+    private void MoveUpRight()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0.75f, 4.5f);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+    private void MoveUpLeft()
+    {
+        if (Input.GetKeyDown("left"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-0.75f, 4.5f);
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+    }
+    private void MoveDownRight()
+    {
+        if (Input.GetKeyDown("right"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0.80f, 2.0f);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+    private void MoveDownLeft()
+    {
+        if (Input.GetKeyDown("down"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-0.75f, 2.0f);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    private void Reset()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            transform.position = new Vector3(-1.0f, 3.0f, -5.224f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 Offset = new Vector3(0.0f, collision.gameObject.GetComponent<BoxCollider2D>().size.y, 0.0f);
+        transform.position = new Vector3(collision.transform.position.x, collision.transform.position.y, transform.position.z) + (Offset * 2);
+    }
+}
